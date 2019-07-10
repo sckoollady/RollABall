a3291e3f11f1f2e4f3f3ca89c3a065e1cc9047d5
Selena <se12na@knights.ucf.edu>
2019-07-09 22:11:47 -0400

    Completed Tier 3.1

---
+++ RollABall/Assets/Scripts/BarrelController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelController : MonoBehaviour
+{
+     private Vector3 pos1 = new Vector3(13f,0.5f,-8f);
+     private Vector3 pos2 = new Vector3(13f,0.5f,8f);
+     public float speed = 1.0f;
+ 
+     void Update() {
+         transform.position = Vector3.Lerp (pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+     }
+}
---
+++ RollABall/Assets/Scripts/BarrelTwo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelTwo : MonoBehaviour
+{
+    // Start is called before the first frame update
+     private Vector3 pos1 = new Vector3(29.6f,0.5f,8.15f);
+     private Vector3 pos2 = new Vector3(29.6f,0.5f,-9f);
+     public float speed = 1.0f;
+ 
+     void Update() {
+         transform.position = Vector3.Lerp (pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+     }
+}
